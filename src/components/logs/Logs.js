712aeda471e5043b320f452f61bec8bc198d0179
Michael Corey Munn <coreymunn3@gmail.com>
2020-11-02 09:25:31 -0500

    collection styling on logs, scaffolding for logItem component. Tried and failed to set up modular scss files.

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
+
+// import styles from './Logs.module.scss';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    const getLogs = async () => {
+      setLoading(true);
+      // request to backend
+      const res = await axios.get('/logs');
+      setLogs(res.data);
+      setLoading(false);
+    };
     getLogs();
     // eslint-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-    // request to backend
-    const res = await axios.get('/logs');
-    setLogs(res.data);
-    setLoading(false);
-  };
   if (loading) {
     return <h4>Loading...</h4>;
   }
-  return <div>Logs</div>;
+  return (
+    <Fragment>
+      <h3 className='title has-text-primary collectionTitle'>System Logs</h3>
+      <ul className='collection'></ul>
+    </Fragment>
+  );
 };
 
 export default Logs;
---
+++ src/components/logs/logItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LogItem = ({ log }) => {
+  return <div>hello</div>;
+};
+
+LogItem.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+export default LogItem;
